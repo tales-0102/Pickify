d1c16b7943ff97455dd36b86fdec31896c89b34a
tales-0102 <talest695@gmail.com>
2026-07-15 12:03:18 +0500

    blod problem solve

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,4 +1,12 @@
 
+// import product from './product'
+// import post from './post'
+// export const schemaTypes = [product]
+
 import product from './product'
 import post from './post'
-export const schemaTypes = [product]
+
+export const schemaTypes = [
+  product,
+  post,
+]
